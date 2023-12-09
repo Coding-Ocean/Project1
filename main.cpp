ab6fd7063e100bfb6d56df0f92567ee7ef9a9e00
Ossan Coding <codingossan@outlook.jp>
2023-12-09 19:46:37 +0900

    add countDown()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+void countDown(int num)
+{
+	for (int i = num; i > 0; i--) {
+		std::cout << i << ' ';
+	}
+	std::cout << std::endl;
+}
 int main()
 {
-	int num = 0;
+	int num = 5;
+	countDown(num);
 	system("pause>0");
 }
