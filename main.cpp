7da1022622ddb233a855f03139a1f8167000d3c1
Coding-Ocean <eijo524@gmail.com>
2023-12-09 19:56:32 +0900

    add loadData()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include<fstream>
+void loadData(const char* filename, int* num)
+{
+	std::ifstream fin(filename);
+	fin >> *num;
+}
 int main()
 {
 	int num = 0;
+	loadData("data.txt", &num);
 	system("pause>0");
 }
